f36c96098f7bd60a941bfa0e435582f44e44d218
stefanlevbidi <stefanlev21796@gmail.com>
2023-02-01 16:31:28 +0000

    saved

--- app.js
+++ app.js
@@ -1,159 +1,99 @@
-// Seattle Shop //---------------------------
-
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-const seattle = {
-  name: "Seattle",
-  minCust: 23,
-  maxCust: 65,
-  avgSale: 6.3,
-  custPerHour: [],
-  getCustPerHour: function() {
-    for(let i = 0; i < hours.length; i++) {
-      let numCusts = randomNum(this.minCust, this.maxCust);
-      this.custPerHour.push(numCusts);
-    }
-    this.calcCookiesPerHour();
-  }
-  calcCookiesPerHour: function(){
-    for(let i = 0; < seattle.custPerHour.length; i++){
-        this.calcCookiesPerHour.push(this.custPerHour[i] * 6.3)
-    }
-  }
-}
-
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-seattle.getCustPerHour();
-console.log(seattle)
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
-const seattleDiv = document.getElementbyID("seattle")
-
-const h2 = document.createElement("h2")
-h2.textContent = seattle.name;
-seattleDiv.appendChild(h2);
-
-for(let i = 0; < seattle.custPerHour.length; i++) {
+function CookieStore(name, minCust, maxCust, avgSale) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSale = avgSale;
+  this.custPerHour = [];
+  this.cookiesPerHour = [];
 }
 
+CookieStore.prototype.calcCustPerHour = function () {
+  for (let i = 0; i < hours.length; i++) {
+    let numCust = randomNum(this.minCust, this.maxCust);
+    this.custPerHour.push(numCust);
+  }
+};
 
+CookieStore.prototype.calcCookiesPerHour = function () {
+  for (let i = 0; i < this.custPerHour.length; i++) {
+    let cookiesSold = Math.floor(this.custPerHour[i] * this.avgSale);
+    this.cookiesPerHour.push(cookiesSold);
+  }
+};
 
+CookieStore.prototype.render = function () {
+  this.calcCustPerHour();
+  this.calcCookiesPerHour();
 
+  // table
+  const table = document.getElementById("myTable");
 
+  // table row
+  const tr = document.createElement("tr");
+  table.appendChild(tr);
 
+  // table cell
+  let td = document.createElement("td");
+  td.textContent = this.name;
+  tr.appendChild(td);
 
+  // get data into the row
+  for (let i = 0; i < this.cookiesPerHour.length; i++) {
+    td = document.createElement("td");
+    td.textContent = this.cookiesPerHour[i];
+    tr.appendChild(td);
+  }
+};
 
+function makeHeaderRow() {
+  // table
+  const table = document.getElementById("myTable");
 
+  // table row
+  const tr = document.createElement("tr");
+  table.appendChild(tr);
 
-
-
-
-
-
-
-
-
-// Tokyo Shop //------------------------------
-
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-const tokyo = {
-  name: "Tokyo",
-  minCust: 3,
-  maxCust: 24,
-  avgSale: 1.2,
-  custPerHour: [],
-  getCustPerHour: function() {
-    for(let i = 0; i < hours.length; i++) {
-      let numCusts = randomNum(this.minCust, this.maxCust);
-      this.custPerHour.push(numCusts)
-    }
+  // starting cell
+  let th = document.createElement("th");
+  tr.appendChild(th);
+  // get data into the row
+  for (let i = 0; i < hours.length; i++) {
+    th = document.createElement("th");
+    th.textContent = hours[i];
+    tr.appendChild(th);
   }
 }
 
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+makeHeaderRow();
 
-seattle.getCustPerHour();
-console.log(tokyo)
-
-// Dubai Shop //--------------------------
-
-
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-const dubai = {
-  name: "Dubai",
-  minCust: 11,
-  maxCust: 38,
-  avgSale: 3.7,
-  custPerHour: [],
-  getCustPerHour: function() {
-    for(let i = 0; i < hours.length; i++) {
-      let numCusts = randomNum(this.minCust, this.maxCust);
-      this.custPerHour.push(numCusts)
-    }
-  }
-}
-
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-seattle.getCustPerHour();
-console.log(dubai)
-
-// Paris Shop //--------------------------
-
-
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-const paris = {
-  name: "Paris",
-  minCust: 20,
-  maxCust: 38,
-  avgSale: 2.3,
-  custPerHour: [],
-  getCustPerHour: function() {
-    for(let i = 0; i < hours.length; i++) {
-      let numCusts = randomNum(this.minCust, this.maxCust);
-      this.custPerHour.push(numCusts)
-    }
-  }
-}
-
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-seattle.getCustPerHour();
-console.log(paris)
-
-// Lima Shop //-----------------------------
-
-
-const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-const lima = {
-  name: "Lima",
-  minCust: 2,
-  maxCust: 16,
-  avgSale: 4.6,
-  custPerHour: [],
-  getCustPerHour: function() {
-    for(let i = 0; i < hours.length; i++) {
-      let numCusts = randomNum(this.minCust, this.maxCust);
-      this.custPerHour.push(numCusts)
-    }
-  }
-}
-
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-seattle.getCustPerHour();
-console.log(lima)
+const seattle = new CookieStore("Seattle", 23, 65, 6.3);
+const tokyo = new CookieStore("Tokyo", 3, 24, 1.2);
+const dubai = new CookieStore("Dubai", 11, 38, 3.7);
+const paris = new CookieStore("Paris", 20, 38, 2.3);
+const lima = new CookieStore("Lima", 2, 16, 4.6);
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
